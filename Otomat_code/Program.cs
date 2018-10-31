183eb89adfb1d37d1ee3546c98e4e29769479eac
hung4564 <hphamviet83@gmail.com>
2018-10-31 17:59:50 +0700

    Add project files.

---
+++ Otomat_code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otomat_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
